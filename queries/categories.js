e1af620ac1d0eb3a264561061f5ccbae99a2b55f
imam0321 <imam.hossain0321@gmail.com>
2025-04-17 01:47:13 +0600

    Refactor EnrolledCoursesPage to fetch and display user enrollments; implement redirection for unauthenticated users and create EnrolledCourseCard component for rendering course details. Add category fetching to EnrolledCourseCard and update course thumbnail handling. Enhance enrollment queries with user-specific data retrieval and add assessment and report models. Update next.config.mjs to allow additional image sources and remove unused python_thumbnail image.

--- queries/categories.js
+++ queries/categories.js
@@ -1,7 +1,14 @@
 import { replaceMongoId } from "@/lib/convertData";
 import { Category } from "@/models/category-model";
 
+// get categories
 export async function getCategories() {
   const categories = await Category.find({}).lean();
   return replaceMongoId(categories);
+}
+
+// get category details
+export async function getCategoryDetails(categoryId) {
+  const category = await Category.findById(categoryId).lean();
+  return replaceMongoId(category);
 }
--- queries/enrollment.js
+++ queries/enrollment.js
@@ -1,10 +1,26 @@
 import { replaceMongoId } from "@/lib/convertData";
+import { Course } from "@/models/course-model";
 import { Enrollment } from "@/models/enrollment-model";
 
 // get enrollments for course Id
 export async function getEnrollmentsForCourse(courseId) {
   const enrollments = await Enrollment.find({ course: courseId }).lean();
   return replaceMongoId(enrollments);
+}
+
+// get enrollments for user
+export async function getEnrollmentsForUser(userId) {
+  try {
+    const enrollments = await Enrollment.find({ student: userId })
+      .populate({
+        path: "course",
+        module: Course,
+      })
+      .lean();
+    return replaceMongoId(enrollments);
+  } catch (error) {
+    throw new Error(error);
+  }
 }
 
 // enrollment course
